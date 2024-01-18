0b4ef0e29e444e0c81fd773ef3b8f9afc30b5fcd
parmar-chirayu <cyparmar@my.yorku.ca>
2024-01-17 20:25:39 -0500

    Practice

---
+++ Customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+var customer = new Customer();
+customer.firstName = "Martin";
+customer.lastName = "Christian";
+console.log(customer.firstName);
+console.log(customer.lastName);
---
+++ Customer.ts
@@ -0,0 +1,12 @@
+class Customer {
+    firstName : String;
+    lastName : String;
+}
+
+let customer = new Customer();
+
+customer.firstName = "Martin";
+customer.lastName = "Christian";
+
+console.log(customer.firstName);
+console.log(customer.lastName);
